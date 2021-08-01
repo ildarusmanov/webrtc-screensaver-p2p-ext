8283837c44426d7edcb84d40d120ee9fabd149be
Ildar <wajox@mail.ru>
2021-08-01 19:07:44 +0300

    add popup/recording window

--- background.js
+++ background.js
@@ -1,43 +1,11 @@
-// frame.setAttribute("allow", "microphone; camera");
-
-// let sd = ''
-// let pc = new RTCPeerConnection({
-//     iceServers: [
-//         {
-//         urls: 'stun:stun.l.google.com:19302'
-//         }
-//     ]
-// })
-
-// var log = msg => {
-//     console.log(`${msg}`)
-// }
-
-// navigator.mediaDevices
-//          .getUserMedia({ video: true, audio: true })
-//          .then(stream => {
-//              //pc.addStream(document.getElementById('video1').srcObject = stream)
-//              pc.createOffer().then(d => pc.setLocalDescription(d)).catch(log)
-//          }).catch(log)
-
-// pc.oniceconnectionstatechange = e => log(pc.iceConnectionState)
-// pc.onicecandidate = event => {
-//     if (event.candidate === null) {
-//         sd = btoa(JSON.stringify(pc.localDescription))
-
-//         console.log(sd);
-//     }
-// }
-
-// init = () => {
-//     if (sd === '') {
-//         return alert('Session Description must not be empty')
-//     }
-
-//     try {
-//         pc.setRemoteDescription(new RTCSessionDescription(JSON.parse(atob(sd))))
-//     } catch (e) {
-//         alert(e)
-//     }
-// }
-console.log('background.js')
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {},
+        })
+        ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+      }]);
+    });
+});
--- recording.js
+++ recording.js
@@ -20,16 +20,16 @@ var log = msg => {
     console.log(`${msg}`)
 }
 
-// navigator.mediaDevices
-//          .getUserMedia({ video: true, audio: true })
-//          .then(stream => {
-//              pc.addStream(document.getElementById('video1').srcObject = stream)
-//              pc.createOffer().then(d => pc.setLocalDescription(d)).catch(log)
-//          }).catch(log)
+navigator.mediaDevices
+         .getUserMedia({ video: true, audio: true })
+         .then(stream => {
+             pc.addStream(document.getElementById('videoCamera').srcObject = stream)
+             pc.createOffer().then(d => pc.setLocalDescription(d)).catch(log)
+         }).catch(log)
     
 navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
          .then(stream => {
-            pc.addStream(document.getElementById('video1').srcObject = stream)
+            pc.addStream(document.getElementById('videoScreen').srcObject = stream)
             pc.createOffer().then(d => pc.setLocalDescription(d)).catch(log)
          })
 
